a1e5b834b4863bed2b2694cbc5b8d90c5a1da59d
kinderproc <kinderproc84@yandex.ru>
2024-06-26 12:51:35 +0300

    initial

---
+++ src/main/java/com/loan/tranche/TrancheApplication.java
@@ -0,0 +1,13 @@
+package com.loan.tranche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrancheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrancheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loan/tranche/TrancheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.tranche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrancheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
