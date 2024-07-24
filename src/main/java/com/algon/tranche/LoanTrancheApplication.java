a9dbe824b4321c6303626afa5d197f72da08f975
kinderproc <kinderproc84@yandex.ru>
2024-07-24 14:06:39 +0300

    feat: initial commit

---
+++ src/main/java/com/algon/tranche/LoanTrancheApplication.java
@@ -0,0 +1,13 @@
+package com.algon.tranche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanTrancheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanTrancheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algon/tranche/LoanTrancheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algon.tranche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanTrancheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
